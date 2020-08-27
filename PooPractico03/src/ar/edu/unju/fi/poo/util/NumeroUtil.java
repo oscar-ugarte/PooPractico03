412596e9a8e19b5f403db82ab7f79c283b8728d7
oscar-ugarte <oscar.ugrt@gmail.com>
2020-08-27 19:00:43 -0300

    Lista la utilidad de numeros que trabaja con el legajos y el salario. Primer metodo permite crear una cadena con el legajo y completar el resto con ceros. El segundo metodo toma un valor double y devuelve un String con la parte redondeada.

---
+++ PooPractico03/src/ar/edu/unju/fi/poo/util/NumeroUtil.java
@@ -0,0 +1,35 @@
+package ar.edu.unju.fi.poo.util;
+
+public class NumeroUtil
+{
+	public static Integer CANTIDAD_DIGITOS_LEGAJO = 11; //Se va usar para armar la cadena con el numero de legajo y ceros.
+	
+	/**
+	 * Recibe un entero como parametro junto a la cantidad de digitos que quiere completar
+	 * con ceros. Devueve una cadida que incluye el legajo y el resto ceros.
+	 * @param legajo
+	 * @param longitud
+	 * @return
+	 */
+	public static String rellenarLegajoConCeros(Integer legajo, Integer cantidadDeDigitos)
+	{
+		String legajoConCeros = "" ;
+		for( Integer i = 1; i <= cantidadDeDigitos - legajo.toString().length() ; i++ )
+		{
+			legajoConCeros = legajoConCeros + "0";
+		}
+		return legajoConCeros + legajo.toString();
+	}
+
+	/**
+	 * Recibe un importe de tipo double y devuelve una cadena sin la pare decimal.
+	 * @param importe
+	 * @return
+	 */
+	public static String redondearImporte(Double importe)
+	{
+		return "$ " + String.format("%.2f", importe) ; 
+	}
+	
+	
+}
